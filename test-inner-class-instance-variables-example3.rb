2798909f6d9ebfa288bba7f188793d7e644c8ce7
Mark D. Blackwell <markdblackwell01@gmail.com>
2009-09-27 22:05:35 -0400

    Added test-inner-class-instance-variables-example3.

---
+++ test-inner-class-instance-variables-example3.rb
@@ -0,0 +1,22 @@
+class A
+  def self.foo
+    @@foo
+  end
+  def self.foo=( v)
+    @@foo = v
+  end
+end
+a = A.new
+class B < A
+end
+b = B.new
+
+a.class.foo = 'bar'
+p a.class.foo
+p b.class.foo
+p A.foo
+
+b.class.foo = 'yuk'
+p a.class.foo
+p b.class.foo
+p B.foo
