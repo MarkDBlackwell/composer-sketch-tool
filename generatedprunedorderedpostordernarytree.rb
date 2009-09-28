1c2a3522d6ad0512afa7ba03ab0aed8ac2e9c57a
Mark D. Blackwell <markdblackwell01@gmail.com>
2009-09-27 22:22:36 -0400

    Changed generatedprunedorderedpostordernarytree.

--- generatedprunedorderedpostordernarytree.rb
+++ generatedprunedorderedpostordernarytree.rb
@@ -1,7 +1,7 @@
 =begin
 Author: Mark D. Blackwell
 Date created: April 24, 2009.
-Date last changed: May 11, 2009.
+Date last changed: May 14, 2009.
 Copyright (c) 2009 Mark D. Blackwell.
 
 Name: Generated, pruned, post-order, ordered, n-ary tree.
@@ -9,12 +9,12 @@ File: generatedprunedorderedpostordernarytree.rb
 Usage: require 'generatedprunedorderedpostordernarytree'
 
 A virtual tree, using depth-first traversal; only a single branch exists at any time. The 
-module, GeneratedPrunedOrderedPostOrderNaryTreeExample, is part of this. This previously used 
+module, GeneratedPrunedOrderedPostOrderNaryTreeExample, is part of this. Previously used 
 the Template Method Design Pattern. See:
 http://sourcemaking.com/design_patterns/template_method
 
 Rather than specialized work being done in subclasses, converted this code to what seems to 
-be the Decorator Design Pattern. Instead, the GeneratedPrunedOrderedPostOrderNaryTree classes 
+be the Decorator Design Pattern. Now, the GeneratedPrunedOrderedPostOrderNaryTree classes 
 accept decoration (?) classes as arguments, the methods of which contain the user's 
 specialization code. See:
 http://sourcemaking.com/design_patterns/decorator
@@ -22,115 +22,175 @@ http://sourcemaking.com/design_patterns/decorator
 Or maybe it should be the Visitor Design Pattern:
 http://sourcemaking.com/design_patterns/visitor
 
-I would like to make this work for more than one tree at once, and the Node class variables 
-are messing this up. Intuition tells me I need a metaclass, singleton class, prototype, or the
-like.
-
-Maybe the solution is to use class instance variables.
-Did so.
+I would like to make this work for more than one tree at once. The Leaf class variables 
+were messing that up. Changed to use class instance variables. Maybe it is good, now?
 =end
 #=========================
 module GeneratedPrunedOrderedPostOrderNaryTree
 #-----------------------------
-  class Node
-# TO-DO: Incorporate here the array of sibling choices, or the calls to methods for
-# determining same.
-    attr_reader :node_decorator
-    class << Node
+  class Leaf
+    class << Leaf
       alias old_new new
+      attr_reader :leaf_decorator_class
+      attr_accessor :processing_leaf
     end
-
+    attr_reader :leaf_decorator,
+                :parent
     public
-    def Node.new( *args)
-      Node.old_new( *args)
-      Node.leaf()
+    def Leaf.new( *args)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf.new'
+      Leaf.old_new( *args)
+# By unwinding the stack, methods, 'Leaf.old_new' and 'initialize' unhelpfully return not the 
+# leaf but the *first* node made, so we instead remember the leaf in the class instance 
+# variable, 'processing_leaf'.
+      Leaf.processing_leaf
     end
 
     public
     def initialize( p, n)
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node#initialize'
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf#initialize'
           @parent = p
-# By unwinding the stack, the method, 'initialize' unhelpfully returns not the leaf but the 
-# *first* object made, so instead track the leaf by the class variable, processing_node.
-      (@node_decorator = n).step_out_branch_leftward( self)
+          @leaf_decorator = n
+      step_out_branch_leftward()
     end
 
-    public
-    def Node.capture_leaf( p)
-      @processing_node = p
+    private
+    def step_out_branch_leftward
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf#step_out_branch_leftward'
+      until @leaf_decorator.candidate().nil?
+        try = @leaf_decorator.data_for_child()
+        @leaf_decorator.next_candidate()
+        next if @leaf_decorator.forbear?( try)
+        Leaf.new( parent = self, Leaf.leaf_decorator_class.new( try))
+        break
+      end #until
+# Assume that before this leaf (self) was created, it was checked regarding forbearance.
+      Leaf.processing_leaf = self if @leaf_decorator.candidate().nil?
+      Leaf.processing_leaf
     end
 
-    public
-    def Node.counts_to_s
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.counts_to_s'
-      '@count ' + @count.inspect + "\n"
+    public # Invoked from another node.
+    def next_candidate
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf#next_candidate'
+      return Leaf.processing_leaf = self if @leaf_decorator.candidate().nil? # No more siblings.
+      try = @leaf_decorator.data_for_child()
+      @leaf_decorator.next_candidate()
+      return next_candidate() if @leaf_decorator.forbear?( try)
+      Leaf.new( parent = self, Leaf.leaf_decorator_class.new( try))
     end #def
 
     public
-    def Node.each( first_leaf)
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.each'
+    def Leaf.each( first_leaf)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf.each'
+# Automatic, depth-first traversal obviates navigating explicitly downward to child leaves.
 #print 'first_leaf '; p first_leaf
-      node = first_leaf
-# Automatic, depth-first traversal obviates navigating explicitly downward to child nodes.
-      (yield node; node = node.create_sibling()) until node.nil?
+      leaf = first_leaf
+      until leaf.nil?
+        yield leaf
+        leaf = leaf.parent.nil? ? nil : leaf.parent.next_candidate()
+      end #until
     end #def
 
-    public
-    def Node.get_first_leaf( initial_node_decorator)
-      Node.new( parent = nil, initial_node_decorator)
+    private
+    def Leaf.get_first_leaf( initial_leaf_decorator)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf.get_first_leaf'
+      Leaf.new( parent = nil, initial_leaf_decorator)
     end
 
     public
-    def Node.increment_count
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.increment_count'
-      @count += 1
+    def Leaf.set_initial( d)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Leaf.set_initial'
+      @leaf_decorator_class = d
     end
-
-    public
-    def Node.leaf
-      @processing_node
-    end
-
-    public
-    def Node.set_initial
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.set_initial'
-      @count = 0
-    end
-
-    public
-    def create_sibling
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node#create_sibling'
-      return nil if @parent.nil?
-      @parent.node_decorator.in_parent_create_next_child( @parent)
-    end
-
   end #class
 #-----------------------------
   class Tree
-    def initialize( t, initial_node_decorator)
+    def initialize( t, initial_leaf_decorator)
 #p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#initialize'
           @tree_decorator = t
-      @first_leaf = Node.get_first_leaf( initial_node_decorator)
+      @first_leaf = Leaf.get_first_leaf( initial_leaf_decorator)
       @has_been_walked = false
     end
 
     public
     def walk
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#walk'
       raise 'Only walkable once' if @has_been_walked # I think.
       @has_been_walked = true
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#walk'
-      Node.each( @first_leaf) {|node| handle( node)}
-      @tree_decorator.walk( counts = Node.counts_to_s())
+      @count = 0
+      Leaf.each( @first_leaf) {|leaf| handle( leaf)}
+      @tree_decorator.walk( @count.to_s)
     end
 
     private
-    def handle( node)
+    def handle( leaf)
 #p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#handle'
-      Node.increment_count()
-      @tree_decorator.handle( node)
+      @count += 1
+      @tree_decorator.handle( leaf)
     end #def
   end #class
 end #module GeneratedPrunedOrderedPostOrderNaryTree
 #=========================
 module GeneratedPrunedOrderedPostOrderNaryTreeExample
+#-----------------------------
+  class LeafDecorator
+
+    def initialize( a)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator#initialize'
+          @absolutes = a
+      @candidate_intervals_index = 0
+    end #def
+
+    public
+    def LeafDecorator.set_fixed( n)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator.set_fixed'
+      @@candidates = (1..4).to_a
+      '@@candidates ' + @@candidates.inspect + "\n"
+    end #def
+
+    public
+    def LeafDecorator.initial
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator.initial'
+      LeafDecorator.new( [0])
+    end
+
+    public
+    def forbear?( a) # Anything bad?
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator#forbear?'
+    end
+
+    public
+    def data_for_child
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator#data_for_child'
+    end
+
+    public
+    def candidate
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator#candidate'
+    end
+
+    public
+    def next_candidate
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::LeafDecorator#next_candidate'
+    end
+
+  end #class LeafDecorator
+#-----------------------------
+  class TreeDecorator
+    def initialize( n)
+      @note_space = n
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::TreeDecorator#initialize'
+      @fixed = LeafDecorator.set_fixed( @note_space)
+      GeneratedPrunedOrderedPostOrderNaryTree::Leaf.set_initial( LeafDecorator)
+    end
+
+    def walk( counts)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::TreeDecorator#walk'
+    end
+
+    def handle( leaf)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTreeExample::TreeDecorator#handle'
+    end #def
+
+  end #class TreeDecorator
 end #module GeneratedPrunedOrderedPostOrderNaryTreeExample
