d110881594642987aaaeb5ec4384604625df4871
Mark D. Blackwell <markdblackwell01@gmail.com>
2009-09-27 22:15:19 -0400

    Changed generatedprunedorderedpostordernarytree.

--- generatedprunedorderedpostordernarytree.rb
+++ generatedprunedorderedpostordernarytree.rb
@@ -1,7 +1,7 @@
 =begin
 Author: Mark D. Blackwell
 Date created: April 24, 2009.
-Date last changed: May 8, 2009.
+Date last changed: May 11, 2009.
 Copyright (c) 2009 Mark D. Blackwell.
 
 Name: Generated, pruned, post-order, ordered, n-ary tree.
@@ -27,20 +27,23 @@ are messing this up. Intuition tells me I need a metaclass, singleton class, pro
 like.
 
 Maybe the solution is to use class instance variables.
+Did so.
 =end
 #=========================
 module GeneratedPrunedOrderedPostOrderNaryTree
 #-----------------------------
   class Node
+# TO-DO: Incorporate here the array of sibling choices, or the calls to methods for
+# determining same.
     attr_reader :node_decorator
     private_class_method :new
 
     def initialize( p, n)
 #p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node#initialize'
           @parent = p
-      (@node_decorator = n).step_out_branch_leftward( self)
 # By unwinding the stack, the method, 'initialize' unhelpfully returns not the leaf but the 
-# *first* object made, so instead track the leaf by the class variable, @@processing_node.
+# *first* object made, so instead track the leaf by the class variable, processing_node.
+      (@node_decorator = n).step_out_branch_leftward( self)
     end
 
     public
@@ -52,9 +55,45 @@ module GeneratedPrunedOrderedPostOrderNaryTree
     end
 
     public
+    def Node.capture_leaf( p)
+      @processing_node = p
+    end
+
+    public
+    def Node.counts_to_s
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.counts_to_s'
+      '@count ' + @count.inspect + "\n"
+    end #def
+
+    public
+    def Node.each( first_leaf)
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.each'
+#print 'first_leaf '; p first_leaf
+      node = first_leaf
+# Automatic, depth-first traversal obviates navigating explicitly downward to child nodes.
+      (yield node; node = node.create_sibling()) until node.nil?
+    end #def
+
+    public
+    def Node.get_first_leaf( initial_node_decorator)
+      Node.make_branch_and_return_leaf( parent = nil, initial_node_decorator)
+    end
+
+    public
+    def Node.increment_count
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.increment_count'
+      @count += 1
+    end
+
+    public
+    def Node.leaf
+      @processing_node
+    end
+
+    public
     def Node.set_initial
 #p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.set_initial'
-      @@count = 0
+      @count = 0
     end
 
     public
@@ -64,64 +103,30 @@ module GeneratedPrunedOrderedPostOrderNaryTree
       @parent.node_decorator.in_parent_create_next_child( @parent)
     end
 
-    public
-    def Node.capture_leaf( p)
-      @@processing_node = p
-    end
-
-    public
-    def Node.leaf
-      @@processing_node
-    end
-
-    public
-    def Node.increment_count
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.increment_count'
-      @@count += 1
-    end
-
-    public
-    def Node.counts_to_s
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Node.counts_to_s'
-      '@@count ' + @@count.inspect + "\n"
-    end #def
   end #class
 #-----------------------------
   class Tree
-    def initialize( t)
+    def initialize( t, initial_node_decorator)
 #p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#initialize'
-      @tree_decorator = t
-    end
-
-    public
-    def each
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#each'
-#print '@tree_decorator.first_leaf '; p @tree_decorator.first_leaf
-      node = @tree_decorator.first_leaf
-# Automatic, depth-first traversal obviates navigating explicitly downward to child nodes.
-      (yield node; node = node.create_sibling()) until node.nil?
-    end #def
-  end #class
-#-----------------------------
-  class Walker
-    def initialize( w, tree_decorator)
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Walker#initialize'
-          @walker_decorator = w
-      @tree = Tree.new( tree_decorator)
+          @tree_decorator = t
+      @first_leaf = Node.get_first_leaf( initial_node_decorator)
+      @has_been_walked = false
     end
 
     public
     def walk
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Walker#walk'
-      @tree.each {|node| handle( node)}
-      @walker_decorator.walk( counts = Node.counts_to_s())
+      raise 'Only walkable once' if @has_been_walked # I think.
+      @has_been_walked = true
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#walk'
+      Node.each( @first_leaf) {|node| handle( node)}
+      @tree_decorator.walk( counts = Node.counts_to_s())
     end
 
     private
     def handle( node)
-#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Walker#handle'
+#p 'in GeneratedPrunedOrderedPostOrderNaryTree::Tree#handle'
       Node.increment_count()
-      @walker_decorator.handle( node)
+      @tree_decorator.handle( node)
     end #def
   end #class
 end #module GeneratedPrunedOrderedPostOrderNaryTree
