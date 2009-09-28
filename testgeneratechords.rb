20b3f515cccb3372c408dcd61b67727386ef29d0
Mark D. Blackwell <markdblackwell01@gmail.com>
2009-09-27 22:18:52 -0400

    Added testgeneratechords.

---
+++ testgeneratechords.rb
@@ -0,0 +1,41 @@
+require 'test/unit'
+require 'generatechords'
+  class TestNote < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestWord < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestChord < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestChordPrint < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestChordAccumulate < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestLeaf < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestTree < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
+
+  class TestWalker < Test::Unit::TestCase
+    def test_it
+    end
+  end #class
