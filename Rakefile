f9f325f30b9c080aad70d5a27526ab1888dfed53
Mark D. Blackwell <markdblackwell01@gmail.com>
2009-12-01 14:47:50 -0500

    In rake, combined mocking tests with 'build' tests.

--- Rakefile
+++ Rakefile
@@ -7,35 +7,31 @@ task :default => [:test]
 
 desc 'Run all the tests'
 task :test do
+  sh "ruby test/test-mock.rb" # Unload mocked classes before other tests.
   require 'rake/runtest'
-  Rake.run_tests 'test*.rb'
-  puts 'Ran tests'
+  Rake.run_tests FileList.new( 'test/test*.rb').exclude( 'test/test-mock.rb')
 end
 
 desc 'Build the duodecimal notespace'
 task :build do
   sh "ruby -e \"require 'build'; Invoke::Build.new( note_space_width = 12)\""
 #  build.rb > build-out.txt'
-  puts 'Built the duodecimal notespace'
 end
 
 desc 'List the duodecimal notespace'
 task :list do
   sh "ruby -e \"require 'list'; Invoke::List.new( note_space_width = 12).run()\""
 #  sh 'ruby list.rb > list-out.txt'
-  puts 'Listed the duodecimal notespace'
 end
 
 desc 'Dump the duodecimal notespace'
 task :dump do
   sh "ruby -e \"require 'dump'; Invoke::Dump.new( note_space_width = 12).run()\""
 #  sh 'ruby dump.rb > dump-out.txt'
-  puts 'Dumped the duodecimal notespace'
 end
 
 desc 'Play the duodecimal notespace'
 task :play do
   sh "ruby -e \"require 'play'; Invoke::Play.new( note_space_width = 12).run()\""
 #  sh 'ruby play.rb > play-out.txt'
-  puts 'Played the duodecimal notespace'
 end
--- test/test-build.rb
+++ test/test-build.rb
@@ -1,31 +1,28 @@
 require 'test/unit'
 require 'build'
-#load File.dirname(__FILE__) + '/example-program.rb'
-
-# A file for tests requiring the unaltered program class, MyFile.
-
-# Example class-instance variable:
-# class C;class << self;attr_accessor :a,:b,:c;end;
-# @a=5;self.b=6;def initialize;self.class.c=7;end;end;
-# C.new;p [C.a,C.b,C.c] #--> [5,6,7]
-
+#-----------------------------
 class HasClassInstanceVariable
   class << self
     attr_accessor :a
   end
-end
-
+end #class HasClassInstanceVariable
+#-----------------------------
 class TestClassInstanceVariable < Test::Unit::TestCase
+# Example class-instance variable:
+# class C;class << self;attr_accessor :a,:b,:c;end;
+# @a=5;self.b=6;def initialize;self.class.c=7;end;end;
+# C.new;p [C.a,C.b,C.c] #--> [5,6,7]
   def setup
     @c = HasClassInstanceVariable
     @c.a = 1
   end
+
   def test_revaluing_class_instance_variable
     @c.a *= 5
     assert_equal( 5, @c.a)
-  end
-end
-
+  end #def
+end #class TestClassInstanceVariable
+#-----------------------------
 class TestMyFileActualWrite < Test::Unit::TestCase
   def setup
     @file_name = 'test-my-file-actual-write.txt' # Similar to class name.
@@ -34,7 +31,7 @@ class TestMyFileActualWrite < Test::Unit::TestCase
   def test_actual_write
     a = read()
     assert_equal( 'aaa', a.last)
-    MyFile.open( @file_name, 'w') {|file| file.write 'bbb'}
+    InvokeUtilities::MyFile.open( @file_name, 'w') {|file| file.write 'bbb'}
 #   Process.exit! # Exit without being trapped by Test::Unit.
     a = read()
     assert_equal( 1, a.length)
@@ -48,5 +45,19 @@ class TestMyFileActualWrite < Test::Unit::TestCase
     result = nil # Expand scope.
     File.open( @file_name, 'r') {|file| result = file.readlines}
     result
+  end #def
+end #class TestMyFileActualWrite
+#-----------------------------
+class TestBuildLogger < Test::Unit::TestCase
+  def setup
+    @file_name = 'log/build.txt'
+    File.open( @file_name, 'r') {|f| @before = f.readlines.length}
+    Invoke::Build.new( 0) # Assume this uses MyLogger to log.
   end
-end
+
+  def test_was_anything_actually_written
+    after = nil
+    File.open( @file_name, 'r') {|f| after = f.readlines.length}
+    assert_equal( 1, after - @before)
+  end #def
+end #class TestLoggerBuild
